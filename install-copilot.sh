dece1aedcff9da2f4ef6a7a9213ae4bb3ae18ad6
Lino Galiana <linogaliana@yahoo.fr>
2024-03-13 11:34:18 +0100

    get from minio

--- install-copilot.sh
+++ install-copilot.sh
@@ -2,8 +2,10 @@
 
 # Download files
 # Copilot
-wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot/1.171.0/vspackage -O copilot.vsix.gz
-wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot-chat/0.13.0/vspackage -O copilot-chat.vsix.gz
+# wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot/1.171.0/vspackage -O copilot.vsix.gz
+# wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot-chat/0.13.0/vspackage -O copilot-chat.vsix.gz
+mc cp s3/lgaliana/config/copilot.vsix copilot.vsix.gz
+mc cp s3/lgaliana/config/copilot-chat.vsix copilot-chat.vsix.gz
 
 # Install extensions
 gunzip copilot.vsix.gz 
