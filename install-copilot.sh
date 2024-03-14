e8c0033700fa17a9b43b331e1d298bc977093aad
Lino Galiana <linogaliana@yahoo.fr>
2024-03-14 21:45:45 +0100

    Update install-copilot.sh

--- install-copilot.sh
+++ install-copilot.sh
@@ -4,14 +4,12 @@
 # Copilot
 # wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot/1.171.0/vspackage -O copilot.vsix.gz
 # wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot-chat/0.13.0/vspackage -O copilot-chat.vsix.gz
-mc cp s3/lgaliana/config/copilot.vsix copilot.vsix.gz
-mc cp s3/lgaliana/config/copilot-chat.vsix copilot-chat.vsix.gz
+mc cp s3/lgaliana/config/copilot.vsix copilot.vsix
+mc cp s3/lgaliana/config/copilot-chat.vsix copilot-chat.vsix
 
 # Install extensions
-gunzip copilot.vsix.gz 
 code-server --install-extension copilot.vsix
 rm copilot.vsix
 
-gunzip copilot-chat.vsix.gz 
 code-server --install-extension copilot-chat.vsix
 rm copilot-chat.vsix
