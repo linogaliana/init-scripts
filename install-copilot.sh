67a9217bd181484801e927ba11cbd53e649a7da8
Lino Galiana <linogaliana@yahoo.fr>
2025-01-14 22:35:56 +0100

    Update install-copilot.sh

--- install-copilot.sh
+++ install-copilot.sh
@@ -6,7 +6,7 @@ code-server --install-extension oderwat.indent-rainbow
 code-server --install-extension pomdtr.excalidraw-editor
 code-server --install-extension tamasfe.even-better-toml
 code-server --install-extension aaron-bond.better-comments
-
+code-server --install-extension github.vscode-github-actions
 
 # Replace default flake8 linter with project-preconfigured ruff
 code-server --uninstall-extension ms-python.flake8
