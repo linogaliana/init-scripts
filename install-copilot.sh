1c51e65f7b1e4561c8a02fabad2ef1127e0fd218
Lino Galiana <linogaliana@yahoo.fr>
2025-06-04 16:19:22 +0200

    wrap tabs

--- install-copilot.sh
+++ install-copilot.sh
@@ -100,6 +100,9 @@ echo "Personal init done."
 jq '. + {
     "workbench.colorTheme": "Default Dark Modern",  # Set the theme
 
+	"workbench.editor.wrapTabs": true,
+
+
     "editor.rulers": [80, 100, 120],  # Add specific vertical rulers
     "files.trimTrailingWhitespace": true,  # Automatically trim trailing whitespace
     "files.insertFinalNewline": true,  # Ensure files end with a newline
